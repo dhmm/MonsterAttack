4f9513a61c561dfce190ae441db1647656bad934
dhmm <dhmm@outlook.com>
2022-01-19 16:54:11 +0200

    done

--- app.js
+++ app.js
@@ -1,28 +1,63 @@
 new Vue({
   el: '#app',
   data: {
-    gameStarted : false,
+    gameStarted : false,    
     userHealth: 100,
-    monsterHealth: 100
+    monsterHealth: 100,
+    logs : [],
   },
   methods: {
     //Game playing functions
     startGame : function() {
-      this.gameStarted = true;     
+      this.userHealth = 100;
+      this.monsterHealth = 100;
+      this.gameStarted = true;    
+      this.logs = []; 
     },
     attackToMonster : function () {
-       var attackPower =  this.getRandomAttackPowerForUser();
-       this.monsterHealth -= attackPower;
-       this.attackFromMonster();
+      if(this.gameStarted) {
+        var attackPower =  this.getRandomAttackPowerForUser();
+        this.monsterHealth -= attackPower;  
+        this.addUserLog('User attacked', attackPower);      
+      }
     },
     specialAttackToMonster : function () {
-      var attackPower = this.getRandomAttackPowerForUser() + this.getRandomAttackPowerForUser();
-      this.monsterHealth -= attackPower;
-      this.attackFromMonster();
+      if(this.gameStarted) {
+        var attackPower = this.getSpecialAttackPowerForUser();
+        this.monsterHealth -= attackPower;    
+        this.addUserLog('User attacked with SPECIAL attack', attackPower);    
+      }
     },
     attackFromMonster : function () {
-      var attackPower =  this.getRandomAttackPowerForMonster();
-      this.userHealth -= attackPower;
+      if(this.gameStarted) {
+        var attackPower =  this.getRandomAttackPowerForMonster();
+        this.userHealth -= attackPower;
+        this.addMonsterLog('Monster attacked', attackPower);
+      }
+    },
+    getFirstAid : function () {      
+      if(this.gameStarted) {
+        var firstAid = 0;
+        if( (100-this.userHealth) > 20) {
+          firstAid = 20;
+        } else {
+          firstAid = 100 - this.userHealth;                    
+        }
+        this.userHealth += firstAid;
+        this.addUserLog('User gets first aid', firstAid); 
+        this.attackFromMonster();        
+      }
+    },
+    //Game functions
+    checkIfGameFinished : function () {
+        if(this.userHealth == 0 ) {
+          alert('Monster wins');
+          this.gameStarted = false;  
+        } else if(this.monsterHealth == 0){
+          alert('You win');
+          this.gameStarted = false;  
+        }
+             
     },
     //General function
     getRandomAttackPowerForUser: function () {
@@ -32,7 +67,7 @@ new Vue({
        }
        return attackPower;
     },
-    getSpecialAttacPowerForUser : function () {
+    getSpecialAttackPowerForUser : function () {
       var attackPower = this.getRandomAttackPowerForUser() + this.getRandomAttackPowerForUser();
       if(attackPower > this.monsterHealth) {
         attackPower = this.monsterHealth;
@@ -48,6 +83,27 @@ new Vue({
     },
     getRandomAttackPower : function() {
       return Math.round((Math.random(1,10))*10);
+    },
+    addUserLog : function(message, power) {
+      this.logs.push({ attackType: "player" , message: message, power: power });
+    },
+    addMonsterLog: function(message, power) {
+      this.logs.push({ attackType: "monster" , message: message, power: power });
+    }
+  },
+  watch: {
+    userHealth : function () { 
+      if(this.gameStarted) {
+        console.log('user health'); 
+        this.checkIfGameFinished();
+      }
+    },
+    monsterHealth : function () { 
+      if(this.gameStarted) {
+        console.log('monster health');       
+        this.checkIfGameFinished();
+        this.attackFromMonster();
+      }
     }
   }
 })
