6097dbd5d3ef1cbc8d6e19561a51fb4c9af9347f
dhmm <dhmm@outlook.com>
2022-01-19 16:15:10 +0200

    first playing version - demo

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 new Vue({
   el: '#app',
   data: {
-
+    gameStarted : false,
+    userHealth: 100,
+    monsterHealth: 100
   },
   methods: {
-
+    //Game playing functions
+    startGame : function() {
+      this.gameStarted = true;     
+    },
+    attackToMonster : function () {
+       var attackPower =  this.getRandomAttackPowerForUser();
+       this.monsterHealth -= attackPower;
+       this.attackFromMonster();
+    },
+    specialAttackToMonster : function () {
+      var attackPower = this.getRandomAttackPowerForUser() + this.getRandomAttackPowerForUser();
+      this.monsterHealth -= attackPower;
+      this.attackFromMonster();
+    },
+    attackFromMonster : function () {
+      var attackPower =  this.getRandomAttackPowerForMonster();
+      this.userHealth -= attackPower;
+    },
+    //General function
+    getRandomAttackPowerForUser: function () {
+      var attackPower =  this.getRandomAttackPower();
+       if(attackPower > this.monsterHealth) {
+         attackPower = this.monsterHealth;
+       }
+       return attackPower;
+    },
+    getSpecialAttacPowerForUser : function () {
+      var attackPower = this.getRandomAttackPowerForUser() + this.getRandomAttackPowerForUser();
+      if(attackPower > this.monsterHealth) {
+        attackPower = this.monsterHealth;
+      }
+      return attackPower;
+    },
+    getRandomAttackPowerForMonster: function () {
+      var attackPower =  this.getRandomAttackPower();
+       if(attackPower > this.userHealth) {
+         attackPower = this.userHealth;
+       }
+       return attackPower;
+    },
+    getRandomAttackPower : function() {
+      return Math.round((Math.random(1,10))*10);
+    }
   }
 })
